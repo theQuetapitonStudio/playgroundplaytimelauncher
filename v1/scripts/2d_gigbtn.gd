787bfe161ddc9c5508173e5ba2d1b251e0cc3572
The Quetapiton Dev <oamost123@gmail.com>
2025-11-27 16:00:27 -0300

    Add files via upload

---
+++ v1/scripts/2d_gigbtn.gd
@@ -0,0 +1,9 @@
+extends Button
+
+func _ready():
+	# conecta o próprio botão ao sinal pressed
+	connect("pressed", self, "_on_button_pressed")
+
+func _on_button_pressed():
+	# caminho da cena alvo (corrija pro seu caminho exato)
+	get_tree().change_scene("res://3d/gig/gig.tscn")
